bbf7994f3beff41463ceaae0bffed9e6fabbce4d
Boss <nonso4emma@yahoo.com>
2023-07-03 00:59:40 +0300

    Refactored images folder for easy accessibility

--- src/app/_components/_icons/projects.tsx
+++ src/app/_components/_icons/projects.tsx
@@ -1,6 +1,6 @@
-import Site1 from "../../../../public/images/site 1.png"
-import Site2 from "../../../../public/images/site 2.png"
-import Site3 from "../../../../public/images/site 3.jpeg"
+import Site1 from "@/app/_images/site 1.png"
+import Site2 from "@/app/_images/site 2.png"
+import Site3 from "@/app/_images/site 3.jpeg"
 
 const ProjectsScreenshotList = {
   Site1,
