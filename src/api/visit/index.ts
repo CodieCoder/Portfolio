a9e1098768929db7d0de0f7b20708c0fa12d4864
Chukwunonso E. N <nonso4emma@yahoo.com>
2025-04-17 00:22:12 +0100

    Update index.ts
    Updated cloud implementation

--- src/api/visit/index.ts
+++ src/api/visit/index.ts
@@ -1,5 +1,5 @@
-import axios from "axios"
-import { IDeviceInfo, IInteraction, IMessage } from "./constants"
+import axios from "axios";
+import { IDeviceInfo, IInteraction, IMessage } from "./constants";
 
 export enum AccessTokenEnum {
   localStorageKey = "access_token",
@@ -10,82 +10,85 @@ export enum AccessTokenEnum {
  * from the localStorage and if there's none, requests for one.
  */
 const ApiClient = axios.create({
-  // baseURL: process.env.NEXT_PUBLIC_SERVER_URL,
+  // baseURL: process.env.NEXT_PUBLIC_API_BASE_URL,
   baseURL:
-    process.env.NEXT_PUBLIC_SERVER_URL ||
-    "https://wandering-red-jeans.cyclic.app",
+    process.env.NEXT_PUBLIC_NGROK_URL ||
+    process.env.NEXT_PUBLIC_API_BASE_URL ||
+    "http://localhost:8000",
   headers: { "Content-Type": "application/json" },
-})
+});
+
+
 
 /**
  * @description - adds access_token to the localStorage
  * @param data - string. The access_token to add to localStorage
  */
 export const addAccessToken = (data: string) => {
-  if (!data) return
+  if (!data) return;
   try {
-    localStorage.setItem(AccessTokenEnum.localStorageKey, JSON.stringify(data))
+    localStorage.setItem(AccessTokenEnum.localStorageKey, JSON.stringify(data));
   } catch (error) {
     //failed
   }
-}
+};
 
 /**
  * @description - gets the access_token from localStorage.
  * @returns - the access_token (string) or false if there's none
  */
 export const getAccessToken = (): string | false => {
-  const token = localStorage.getItem(AccessTokenEnum.localStorageKey)
+  const token = localStorage.getItem(AccessTokenEnum.localStorageKey);
   if (token) {
-    return token
+    return token;
   } else {
-    return false
+    return false;
   }
-}
+};
 
 export const firstVisit = async (deviceInfo: IDeviceInfo) => {
   try {
     const addVisitor = await ApiClient.post("visitors", {
       deviceInfo,
-    })
+    });
     if (addVisitor.data) {
-      addAccessToken(addVisitor.data?.access_token)
-      return addVisitor.data?.access_token
+      addAccessToken(addVisitor.data?.access_token);
+      return addVisitor.data?.access_token;
     }
   } catch (error) {
-    return
+    return;
   }
-}
+};
 
 export const apiPost = async (
   url: string,
   data: IInteraction | IMessage,
   deviceInfo: IDeviceInfo
 ) => {
-  const token = getAccessToken()
+  const token = getAccessToken();
   if (token) {
     const config = {
       headers: { Authorization: "Bearer " + token },
-    }
+    };
     try {
-      await ApiClient.post(url, data, config)
-      return true
+      await ApiClient.post(url, data, config);
+      return true;
     } catch (error) {
-      return false
+      return false;
     }
   } else {
-    const another_token = await firstVisit(deviceInfo)
+    const another_token = await firstVisit(deviceInfo);
     if (another_token) {
       const config = {
         headers: { Authorization: "Bearer " + another_token },
-      }
+      };
       try {
-        await ApiClient.post(url, data, config)
-        return true
+        await ApiClient.post(url, data, config);
+        return true;
       } catch (error) {
-        return false
+        return false;
       }
     }
   }
-  return false
-}
+  return false;
+};
